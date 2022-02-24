be99f5a543f8f3f18e090262906276922a2d726d
ahmadkatrib13 <ahmadkatrib123@gmail.com>
2022-02-24 12:27:39 +0200

    step4

---
+++ step4.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Ahmad";
+$position = "Full Stack Developper";
+$github_url = "https://github.com/ahmadkatrib13";
+
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url.".\n";
+echo "Hello, I'm ",$name,", I'm a ",$position," please check my github link ",$github_url,".\n";
+echo "Hello, I'm ${name}, I'm a ${position} please check my github link ${github_url}.\n";
