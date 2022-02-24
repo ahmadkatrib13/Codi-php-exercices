6cd092d905740c5b76a874fb9dcc97c6d2adfdbc
ahmadkatrib13 <ahmadkatrib123@gmail.com>
2022-02-24 11:50:34 +0200

    step 2

---
+++ step2.php
@@ -0,0 +1,9 @@
+Hello world, Help me I am using PHP
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "I am getting errors but I can't see the errors\n";
+print "I am getting errors but I can't see the errors\n";
+?>
