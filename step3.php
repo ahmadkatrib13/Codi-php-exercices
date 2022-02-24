eec5114d387540f5312f80d5ffa8d1dae07c1a1f
ahmadkatrib13 <ahmadkatrib123@gmail.com>
2022-02-24 12:13:19 +0200

    Step 3

---
+++ step3.php
@@ -0,0 +1,10 @@
+<?php
+$name = 'Ahmad Katrib';
+$email = 'ahmadkatrib123@gmail.com';
+
+echo  "My name is ${name} and my email is ${email} .\n";
+
+$height  = 176;
+$width = 156;
+$area = $height*$width;
+echo $area;
